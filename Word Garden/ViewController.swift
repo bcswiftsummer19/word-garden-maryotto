272b8e27a2f657971a20ed0c6b4c22ec7f8f0bb6
Mary Otto <ottomc@bc.edu>
2019-06-09 23:10:17 +0100

    3.2 Complete

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var guessedLetterButton: UIButton!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        guessedLetterButton.isEnabled = false
     }
-
-
+    
+    func updateUIAfterGuess(){
+        guessedLetterField.resignFirstResponder()
+        guessedLetterField.text = ""
+    }
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+//        print("Hey! The guessedLetterFieldChanged!!!")
+        if let letterGuessed = guessedLetterField.text?.last {
+            guessedLetterField.text = "\(letterGuessed)"
+            guessedLetterButton.isEnabled = true
+        } else {
+            // disable the button if I don't have a single character in the guessedLetterField
+            guessedLetterButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        
+//       print("In doneKeyPressed, is guessedLetterField the first responder before updateUIAfterGuess?")
+        
+        updateUIAfterGuess()
+        
+//         print("In doneKeyPressed, is guessedLetterField the first responder after updateUIAfterGuess?")
+    }
+    
+    
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        
+//         print("In guessLetterButtonPressed, is guessedLetterField the first responder before updateUIAfterGuess?")
+        
+       updateUIAfterGuess()
+        
+//         print("In guessLetterButtonPressed, is guessedLetterField the first responder after updateUIAfterGuess?")
+    }
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
